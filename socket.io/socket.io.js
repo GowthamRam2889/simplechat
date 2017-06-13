d5cdaee0cbc81b51f01dd3219a5323c3b5289d86
Gowtham <gowtham.rajaram@pearson.com>
2017-06-12 22:55:09 -0600

    initial commit

---
+++ socket.io/socket.io.js
@@ -0,0 +1,6 @@
+io.sockets.on('connection', function (socket) {
+    socket.emit('message', { message: 'welcome to the chat' });
+    socket.on('send', function (data) {
+        io.sockets.emit('message', data);
+    });
+});
